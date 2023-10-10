e9f3d2746730a1f633b83b2ff7ceea92b2e08783
jason <qqertytt258@gmail.com>
2023-10-10 17:45:57 +0800

    change SetWriteDeadline and set  defer close ticker

--- internal/entities/client.go
+++ internal/entities/client.go
@@ -10,9 +10,10 @@ import (
 )
 
 const (
-	pongWait   = 60 * time.Second
-	pingPeriod = 54 * time.Second
-	writeWait  = 10 * time.Second
+	pongWait    = 60 * time.Second
+	pingPeriod  = 54 * time.Second
+	writeWait   = 10 * time.Second
+	readMaxSize = 1024
 )
 
 var (
@@ -41,7 +42,7 @@ func (c *Client) Read() {
 		c.hub.unregister <- c //不讀取就註銷該client,可能是瀏覽器關閉或網路斷線
 		c.conn.Close()
 	}()
-
+	c.conn.SetReadLimit(readMaxSize)                 //設置每次讀取前端給的訊息大小,超過會報錯
 	c.conn.SetReadDeadline(time.Now().Add(pongWait)) //設定 超時時間
 
 	c.conn.SetPongHandler(func(appData string) error { //當client端 pong 回來 所要做的動作
@@ -88,11 +89,10 @@ func (c *Client) Write() {
 		case msg, ok := <-c.send: //將client端的訊息拿出來
 			if !ok { //如果是false 代表client 可能斷線或關閉瀏覽器
 				log.Printf("client端讀取send channel 失敗")
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{}) // 通知client端關閉訊息
-				return
-			}
-			if err := c.conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil { //設定超時時間
-				log.Printf("SetWriteDeadline error:%s \n", err.Error())
+				err := c.conn.WriteMessage(websocket.CloseMessage, []byte{}) // 通知client端關閉訊息
+				if err != nil {
+					log.Printf("WriteMessage for closeMessage error:%s \n", err.Error())
+				}
 				return
 			}
 			writer, err := c.conn.NextWriter(websocket.TextMessage) //創建寫入器,方便多次寫入
@@ -100,7 +100,17 @@ func (c *Client) Write() {
 				log.Printf("NextWriter error:%s", err.Error())
 				return
 			}
-			if _, err = writer.Write(msg); err != nil { //發送訊息到client端
+			defer func() {
+				if err = writer.Close(); err != nil { //使用完記得關閉writer,釋放資源
+					log.Printf("write close error:%s \n", err.Error())
+					return
+				}
+			}()
+			if err := c.conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil { //設定超時時間,這邊是設置10秒,超過就會中斷websocket連線
+				log.Printf("SetWriteDeadline error:%s \n", err.Error())
+				return
+			}
+			if _, err = writer.Write(msg); err != nil { //發送訊息到client端,如果超時會回報錯誤
 				log.Printf("write msg error :%s\n", err.Error())
 				return
 			}
@@ -120,11 +130,6 @@ func (c *Client) Write() {
 					log.Printf("write send message error :%s \n", err.Error())
 					return
 				}
-			}
-
-			if err = writer.Close(); err != nil { //使用完記得關閉writer,釋放資源
-				log.Printf("write close error:%s \n", err.Error())
-				return
 			}
 
 		case <-ticker.C: //計時器,為了定時做某些事情
